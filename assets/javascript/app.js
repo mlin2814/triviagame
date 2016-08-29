7eae2f133cfe2935ba6a059c2240bffa1fd0eaa5
Matthew Lin <Matthew Lin>
2016-08-29 11:57:31 -0400

    Updated js file to have a working countdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,33 @@
+window.onload = function(){
+    $('#stop').on('click', stopwatch.stop);
+    $('#reset').on('click', stopwatch.reset);
+    $('#start').on('click', stopwatch.start);
+};
+
+// Our stopwatch object
 var stopwatch = {
-    time:0,
+    time:5,
     reset: function(){
         stopwatch.time = 0;
         // DONE: Change the "display" div to "00:00."
-        $('#display').html('00:00');
+        $('#display').html('20:00');
         // DONE: Empty the "laps" div.
     },
     start: function(){
         // DONE: Use setInterval to start the count here.
-        counter = setInterval(stopwatch.count, 300000);
+        counter = setInterval(stopwatch.count, 1000);
     },
     stop: function(){
         // DONE: Use clearInterval to stop the count here.
         clearInterval(counter);
     },
- 	function(){   
-        var converted = stopwatch.timeConverter(stopwatch.time);
-        // DONE: Add the current lap and time to the "laps" div.
-    },
     count: function(){
         // DONE: increment time by 1, remember we cant use "this" here.
         stopwatch.time--;
+        if (stopwatch.time < 0){
+            return;
+        }
+
         // DONE: Get the current time, pass that into the stopwatch.timeConverter function, 
         //       and save the result in a variable.
         var converted = stopwatch.timeConverter(stopwatch.time);
@@ -40,65 +47,95 @@ var stopwatch = {
         }
         return minutes + ":" + seconds;
     }
-
-    var startAt = 10;
-        var number = startAt;
-
-        // When the stop button gets clicked, run the stop function.
-        $('#stop').on('click', stop);
-        // When the resume button gets clicked, execute the run function.
-        $('#resume').on('click', run);
-        $('#reset').on('click', run);
-
-        // The run function sets an interval
-        // that runs the decrement function once a second.
-        // Notice how we name the interval "counter."
-        function displayNumber(){
-            $('#show-number').html('<h2>' + number + '</h2>');
-        }
-        function run(){
-            counter = setInterval(decrement, 1000);
-        }
-
-function reset (){
-    number=startAt;
-    displayNumber();
-}
-
-        // The decremeent function.
-        function decrement(){
-            // Decrease number by one.
-            number--;
-            // Show the number in the #show-number tag.
-            $('#show-number').html('<h2>' + number + '</h2>');
-
-            // Once number hits zero...
-            if (number === 0){
-                // ...run the stop function.
-                stop();
-                // Alert the user that time is up.
-                alert('Time Up!')
-            }
-        }
-
-        // The stop function
-        function stop(){
-            // Clears our "counter" interval.
-            // We just pass the name of the interval
-            // to the clearInterval function.
-            clearInterval(counter);
-        }
-
-        // Execute the run function.
-        run();
 };
+//    var startAt=2000;
+//         var number = startAt;
 
-question1: function(){
-var question1 = "We are the ...";
-var answers = ["Crystal Gems", "Champions", "World", "99%"];
-var answer = "Crystal Gems";
-for (var i = 0; i<answers.length; i++) {
-	Things[i]
-}
-	if
-}
+//         // When the stop button gets clicked, run the stop function.
+//         $('#stop').on('click', stop);
+//         // When the resume button gets clicked, execute the run function.
+//         $('#resume').on('click', run);
+//         $('#reset').on('click', reset);
+
+//         // The run function sets an interval
+//         // that runs the decrement function once a second.
+//         // Notice how we name the interval "counter."
+//         function displayNumber(){
+//             $('#show-number').html('<h2>' + number + '</h2>');
+
+//         }
+
+//         function run(){
+//             counter = setInterval(decrement, 1000);
+//         }
+
+//         function reset() {
+//             number=startAt;
+//             displayNumber();
+//         }
+
+//         // The decremeent function.
+//         function decrement(){
+//             // Decrease number by one.
+//             number--;
+//             // Show the number in the #show-number tag.
+//             displayNumber();
+//             // Once number hits zero...
+//             if (number === 0){
+//                 // ...run the stop function.
+//                 stop();
+// //                number=10;
+//                 // Alert the user that time is up.
+//                 alert('Time Up!')
+//             }
+//         }
+
+//         // The stop function
+//         function stop(){
+//             // Clears our "counter" interval.
+//             // We just pass the name of the interval
+//             // to the clearInterval function.
+//             clearInterval(counter);
+//         }
+
+//         // Execute the run function.
+//         run();
+
+// question1: function(){
+// var question1 = "We are the ...";
+// var answers = ["Crystal Gems", "Champions", "World", "99%"];
+// var answer = "Crystal Gems";
+// for (var i = 0; i<answers.length; i++) {
+// 	Things[i]
+// }
+// if $('.button').click(answers[0])
+//     console.log(Correct!);
+// } else {
+//     console.log(Whoops!);
+// }
+
+// question2: function(){
+// var question2 = "Steven cannot do which one:";
+// var answers = ["Generate cats", "Float", "Heal", "train Lion"];
+// var answer = "train Lion";
+// for (var i = 0; i<answers.length; i++) {
+//     Things[i]
+// }
+// if $('.button').click(answers[0])
+//     console.log(Correct!);
+// } else {
+//     console.log(Whoops!);
+// }
+
+// question1: function(){
+// var question1 = "We are the ...";
+// var answers = ["Crystal Gems", "Champions", "World", "99%"];
+// var answer = "Crystal Gems";
+// for (var i = 0; i<answers.length; i++) {
+//     Things[i]
+// }
+// if $('.button').click(answers[0])
+//     console.log(Correct!);
+// } else {
+//     console.log(Whoops!);
+// }
